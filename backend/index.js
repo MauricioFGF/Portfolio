870eb29a283cdf71c79d8a25804e9c55ce88be35
MauricioFGF <mfgf@cin.ufpe.br>
2023-05-06 19:24:21 -0300

    Adjusts

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const http = require("http");
 const cookieParser = require("cookie-parser");
 const routes = require("./routes/router");
 const mongoose = require("mongoose");
+require("dotenv").config();
 // const conn = require("./db/conn");
 
 app.use(cors());
@@ -13,14 +13,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use("/api", routes);
 
-const server = http.createServer(app);
-
-const mongo_url =
-  "mongodb+srv://mauricio:Mamaelinda1!@portfolio.svmwxzm.mongodb.net/?retryWrites=true&w=majority";
+const mongo_url = process.env.MONGO_URL;
 
 mongoose.connect(mongo_url).then(() => {
   console.log("Conectado ao DB");
-  server.listen(5000, () => {
+  app.listen(5000, () => {
     console.log("Servidor On!");
   });
 });
