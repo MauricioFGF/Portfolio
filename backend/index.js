624940bdc0ed20b5e6643545980258a9bf1c1846
MauricioFGF <mfgf@cin.ufpe.br>
2023-05-06 19:01:48 -0300

    adjust to vercel

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,17 @@ const cors = require("cors");
 const app = express();
 const http = require("http");
 const cookieParser = require("cookie-parser");
+const routes = require("./routes/router");
+const conn = require("./db/conn");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-const routes = require("./routes/router");
 app.use("/api", routes);
 
-const server = http.createServer(app);
-
-const conn = require("./db/conn");
 conn().then(() => {
-  server.listen(3000, function () {
+  app.listen(3000, function () {
     console.log("Servidor On!");
   });
 });
